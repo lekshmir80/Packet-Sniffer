bde709e34138e52de2b52b756d816f901323f6c6
lekshmi <lekshmirajendran123@gmail.com>
2021-09-09 10:48:18 +0530

    Turned to Command line

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -5,23 +5,27 @@ from datetime import datetime
 import sys
 import subprocess #Create another processs
 from scapy.all import *
+from rich.console import Console
 
-net_iface = input("Enter interface name: ")
+console = Console()
+
+
+net_iface = str(sys.argv[1])
 
 subprocess.call(["ifconfig",net_iface,"promisc"]) #creating another process to run command
 
-num_of_pkt = int(input("Enter the packet count you want to capture"))
+num_of_pkt = int(sys.argv[2])
 
-time_sec =int(input("Enter the time how long(in sec) run to capture"))
+time_sec =int(sys.argv[3])
 
-proto = input("Enter the protocol(arp | icmp |all)")
+proto = str(sys.argv[4])
 
 #sniff function call it and pass every packet in byte format
 def logs(packet):
 	print("__________________________________________________________________")
 	#print(packet.show())
-	print(f"SRC_MAC: {str(packet[0].src)} DEST_MAC: {str(packet[0].dst)} TYPE: {str(packet[0].type)}")
-	print(f"psrc: {str(packet[1].psrc)} hwsrc: {str(packet[1].hwsrc)} pdst: {str(packet[1].pdst)}")
+	console.print(f"SRC_MAC: {str(packet[0].src)} DEST_MAC: {str(packet[0].dst)} TYPE: {str(packet[0].type)}",style="bold red")
+	console.print(f"psrc: {str(packet[1].psrc)} hwsrc: {str(packet[1].hwsrc)} pdst: {str(packet[1].pdst)}",style="bold blue")
 	
 if proto == "all":
 	sniff(iface = net_iface ,count = num_of_pkt, timeout = time_sec, prn=logs ) #sniffing packet
